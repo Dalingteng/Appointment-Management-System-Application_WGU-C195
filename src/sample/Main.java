9cd364225ad41f0159ae27bd3dcd4bb35f1479d1
Dalingteng <sochandalingteng@gmail.com>
2023-05-19 12:56:37 -0600

    Second Commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.database.JDBC;
 
 public class Main extends Application {
 
---
+++ src/sample/database/UserDao.java
@@ -0,0 +1,22 @@
+package sample.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+    public static boolean checkLogIn(String userName, String password) throws SQLException {
+        Connection conn = JDBC.getConnection();
+        String sqlStatement = "SELECT * FROM users WHERE User_Name = ? and Password = ?";
+
+        PreparedStatement ps = JDBC.getPreparedStatement();
+        ps.setString(1, userName);
+        ps.setString(2, password);
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()) {
+            return true;
+        }
+        return false;
+    }
+}
