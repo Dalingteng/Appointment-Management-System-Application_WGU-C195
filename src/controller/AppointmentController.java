32d8a03a3d287ab3bd928f02a204f735348e80d8
Dalingteng <sochandalingteng@gmail.com>
2023-05-22 12:58:40 -0600

    Second Commit

--- src/controller/AppointmentController.java
+++ src/controller/AppointmentController.java
@@ -1,5 +1,59 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class AppointmentController {
 
+    public DatePicker datePicker;
+    public RadioButton weekRadioButton;
+    public RadioButton monthRadioButton;
+    public RadioButton allRadioButton;
+    public TableView appointmentTable;
+    public TableColumn appointmentIdColumn;
+    public TableColumn titleColumn;
+    public TableColumn descriptionColumn;
+    public TableColumn locationColumn;
+    public TableColumn contactColumn;
+    public TableColumn typeColumn;
+    public TableColumn startDateColumn;
+    public TableColumn endDateColumn;
+    public TableColumn startTimeColumn;
+    public TableColumn endTimeColumn;
+    public TableColumn customerIdColumn;
+    public TableColumn userIdColumn;
+    public Button customerButton;
+    public Button reportButton;
+    public Button logOutButton;
+    public Button addAppointmentButton;
+    public Button modifyAppointmentButton;
+    public Button deleteAppointmentButton;
+    public Label timeZoneLabel;
+
+    public void onWeekRadioButton(ActionEvent actionEvent) {
+    }
+
+    public void onMonthRadioButton(ActionEvent actionEvent) {
+    }
+
+    public void onAllRadioButton(ActionEvent actionEvent) {
+    }
+
+    public void onCustomerButton(ActionEvent actionEvent) {
+    }
+
+    public void onReportButton(ActionEvent actionEvent) {
+    }
+
+    public void onLogOutButton(ActionEvent actionEvent) {
+    }
+
+    public void onAddAppointmentButton(ActionEvent actionEvent) {
+    }
+
+    public void onModifyAppointmentButton(ActionEvent actionEvent) {
+    }
+
+    public void onDeleteAppointmentButton(ActionEvent actionEvent) {
+    }
 }
--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,4 +1,43 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CustomerController {
+    public TableView customerTable;
+    public TableColumn customerIdColumn;
+    public TableColumn customerNameColumn;
+    public TableColumn addressColumn;
+    public TableColumn phoneNumberColumn;
+    public TableColumn postalCodeColumn;
+    public TableColumn stateProvinceColumn;
+    public TableColumn countryColumn;
+    public Button appointmentButton;
+    public Button reportButton;
+    public Button logOutButton;
+    public Button addCustomerButton;
+    public Button modifyCustomerButton;
+    public Button deleteCustomerButton;
+    public Label timeZoneLabel;
+
+    public void onAppointmentButton(ActionEvent actionEvent) {
+    }
+
+    public void onReportButton(ActionEvent actionEvent) {
+    }
+
+    public void onLogOutButton(ActionEvent actionEvent) {
+    }
+
+    public void onAddCustomerButton(ActionEvent actionEvent) {
+    }
+
+    public void onModifyCustomerButton(ActionEvent actionEvent) {
+    }
+
+    public void onDeleteCustomerButton(ActionEvent actionEvent) {
+    }
 }
