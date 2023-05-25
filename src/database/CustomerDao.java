ac4d4dccd7d1f09960dd86d96d816b307087039c
Dalingteng <sochandalingteng@gmail.com>
2023-05-25 11:23:59 -0600

    Second Commit

--- src/database/CustomerDao.java
+++ src/database/CustomerDao.java
@@ -8,20 +8,30 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CustomerDao {
-//    public static ObservableList<Customer> getAllCustomers() throws SQLException {
-//        String sql = "SELECT * FROM countries";
-//        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
-//        ResultSet rs = ps.executeQuery();
-//        ObservableList<Customer> allCustomers = FXCollections.observableArrayList();
-//        while (rs.next()) {
-//            int customerId = rs.getInt("Customer_ID");
-//            String customerName = rs.getString("Customer_Name");
-//            String address = rs.getString("Address");
-//            String postalCode = rs.getString("Postal_Code");
-//            String phoneNumber = rs.getString("Phone");
-//
-//            allCustomers.add(new Customer());
-//        }
-//        return allCustomers;
-//    }
+    public static ObservableList<Customer> getAllCustomers() throws SQLException {
+        String sql = "SELECT * FROM customers, first_level_divisions, countries WHERE" +
+                "customers.Division_ID = first_level_divisions.Division_ID AND" +
+                "first_level_divisions.Country_ID = countries.Country_ID";
+        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        ObservableList<Customer> allCustomers = FXCollections.observableArrayList();
+        while (rs.next()) {
+            int customerId = rs.getInt("Customer_ID");
+            String customerName = rs.getString("Customer_Name");
+            String address = rs.getString("Address");
+            String postalCode = rs.getString("Postal_Code");
+            String phoneNumber = rs.getString("Phone");
+            int divisionId = rs.getInt("Division_ID");
+            String divisionName = rs.getString("Division");
+            int countryId = rs.getInt("Country_ID");
+            String countryName = rs.getString("Country");
+            allCustomers.add(new Customer(customerId, customerName, address, postalCode, phoneNumber, divisionId, divisionName, countryId, countryName));
+        }
+        return allCustomers;
+    }
+
+    public int addCustomer(String customerName, String address, String postalCode, String phoneNumber, int divisionId) {
+        int rowsAffected = 0;
+        String sql = "INSERT INTO customers";
+    }
 }
--- src/database/ReportDao.java
+++ src/database/ReportDao.java
@@ -1,4 +1,24 @@
 package database;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Report;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ReportDao {
+    public ObservableList<Report> getReportsByMonthType() throws SQLException {
+        String sql = "SELECT * FROM appointments";
+        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        ObservableList<Report> reportsByMonthType = FXCollections.observableArrayList();
+        while (rs.next()) {
+            String month = rs.getString("Month");
+            String type = rs.getString("Type");
+            int monthTypeCount = rs.getInt("Count");
+            reportsByMonthType.add(new Report(month, type, monthTypeCount));
+        }
+        return reportsByMonthType;
+    }
 }
