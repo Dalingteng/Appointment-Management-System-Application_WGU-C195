690b47cfe82195cbbfbc74044a5f2e06b7a69019
Dalingteng <sochandalingteng@gmail.com>
2023-06-05 16:11:04 -0600

    JavaDoc Added

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -63,17 +63,17 @@ public class Appointment {
     /**
      * Creates a new object of Appointment class.
      * @param appointmentId the id of appointment
-     * @param title
-     * @param description
-     * @param location
-     * @param type
-     * @param startDate
-     * @param endDate
-     * @param startTime
-     * @param endTime
-     * @param customerId
-     * @param userId
-     * @param contactId
+     * @param title the title of appointment
+     * @param description the description of appointment
+     * @param location the location of appointment
+     * @param type the type of appointment
+     * @param startDate the start date of appointment
+     * @param endDate the end date of appointment
+     * @param startTime the start time of appointment
+     * @param endTime the end time of appointment
+     * @param customerId the id of customer of appointment
+     * @param userId the id of user of appointment
+     * @param contactId the id of contact of appointment
      */
     public Appointment(int appointmentId, String title, String description, String location, String type,
                        LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime,
@@ -92,102 +92,177 @@ public class Appointment {
         this.contactId = contactId;
     }
 
+    /**
+     * @return the id of appointment
+     */
     public int getAppointmentId() {
         return appointmentId;
     }
 
+    /**
+     * @param appointmentId the id of appointment
+     */
     public void setAppointmentId(int appointmentId) {
         this.appointmentId = appointmentId;
     }
 
+    /**
+     * @return the title of appointment
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * @param title the title of appointment
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * @return the description of appointment
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * @param description the description of appointment
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * @return the location of appointment
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * @param location the location of appointment
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
+    /**
+     * @return the type of appointment
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * @param type the type of appointment
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * @return the start date of appointment
+     */
     public LocalDate getStartDate() {
         return startDate;
     }
 
+    /**
+     * @param startDate the start date of appointment
+     */
     public void setStartDate(LocalDate startDate) {
         this.startDate = startDate;
     }
 
+    /**
+     * @return the end date of appointment
+     */
     public LocalDate getEndDate() {
         return endDate;
     }
 
+    /**
+     * @param endDate the end date of appointment
+     */
     public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
     }
 
+    /**
+     * @return the start time of appointment
+     */
     public LocalTime getStartTime() {
         return startTime;
     }
 
+    /**
+     * @param startTime the start time of appointment
+     */
     public void setStartTime(LocalTime startTime) {
         this.startTime = startTime;
     }
 
+    /**
+     * @return the end time of appointment
+     */
     public LocalTime getEndTime() {
         return endTime;
     }
 
+    /**
+     * @param endTime the end time of appointment
+     */
     public void setEndTime(LocalTime endTime) {
         this.endTime = endTime;
     }
 
+    /**
+     * @return the id of customer of appointment
+     */
     public int getCustomerId() {
         return customerId;
     }
 
+    /**
+     * @param customerId the id of customer of appointment
+     */
     public void setCustomerId(int customerId) {
         this.customerId = customerId;
     }
 
+    /**
+     * @return the id of user of appointment
+     */
     public int getUserId() {
         return userId;
     }
 
+    /**
+     * @param userId the id of user of appointment
+     */
     public void setUserId(int userId) {
         this.userId = userId;
     }
 
+    /**
+     * @return the id of contact of appointment
+     */
     public int getContactId() {
         return contactId;
     }
 
+    /**
+     * @param contactId the id of contact of appointment
+     */
     public void setContactId(int contactId) {
         this.contactId = contactId;
     }
 
+    /**
+     * @return the auto-generated appointment id
+     */
     public static int getAutoAppointmentId(){
         return autoAppointmentId++;
     }
